49577fe2e3b9f6c4cfde8ea0ff85529b1f8f8cf2
Josi <JosiGrc@users.noreply.github.com>
2019-09-25 10:02:11 -0500

    added PeopleController with all needed methods, added all needed views. Curretnly testing that all views and methods work properly and show the proper info to the user.

---
+++ Dating/Dating/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Dating.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Sex { get; set; }
+        public string SexualOrientation { get; set; }
+        public int Location { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
